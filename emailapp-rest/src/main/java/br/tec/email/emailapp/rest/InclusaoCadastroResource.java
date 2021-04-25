1f3ca7f1a5bc1c7e46d83d77b2bbd2fbd612a5bb
Sidney Senna <sidney.senna@gmail.com>
2021-04-25 21:54:51 +0000

    feat: add camada rest

---
+++ emailapp-rest/src/main/java/br/tec/email/emailapp/rest/InclusaoCadastroResource.java
@@ -0,0 +1,45 @@
+package br.tec.email.emailapp.rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import br.tec.email.emailapp.service.inclusaocadastro.InclusaoCadastroService;
+import br.tec.email.emailapp.service.inclusaocadastro.dto.ClienteInclusaoDTO;
+import br.tec.fidelis.ten.service.recebimentoaluguel.dto.ContratoListagemDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(value = "InclusaoCadastro")
+@Path("/recebimento-aluguel")
+public class InclusaoCadastroResource {
+	
+	@Inject
+	private InclusaoCadastroService inclusaoCadastroService;
+
+	public InclusaoCadastroResource(InclusaoCadastroService inclusaoCadastroService) {
+		this.inclusaoCadastroService = inclusaoCadastroService;
+	}
+	
+	@ApiOperation(value = "Cadastrar cliente com endereço")
+	@ApiResponses({ @ApiResponse(code = 200, message = "Registro cadastrado", response = ContratoListagemDTO.class), @ApiResponse(code = 412, message = "Erro", response = String.class) })
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response cadastrarCliente(ClienteInclusaoDTO cliente) {
+		try {
+			inclusaoCadastroService.cadastrarCliente(cliente);
+		} catch (Exception e) {
+			return Response.status(403).build();
+		}
+		
+		return Response.status(201).build();
+	}
+
+}
